52956f8a4a4369b4ed623c85b9109a33d3fcbf58
Varnika <vvarnika05@gmail.com>
2025-07-14 02:05:31 +0530

    add-Counter

--- src/App.jsx
+++ src/App.jsx
@@ -8,55 +8,27 @@ import ShoppingList from './ShoppingList'
 import PropertyList from './PropertyList'
 import ColourBox from './ColourBox'
 import ColourBoxList from './ColourBoxList'
+import Counter from './counter'
 
 
-const colors = [
-  "#E53935",
-  "#E91E63",
-  "#9C27B0",
-  "#673AB7",
-  "#3F51B5",
-  "#2196F3",
-  "#03A9F4",
-  "#00BCD4",
-  "#009688",
-  "#4CAF50",
-  "#8BC34A",
-  "#CDDC39",
-  "#FFEB3B",
-  "#FFC107",
-  "#FF9800",
-  "#FF5722",
-];
 
-const data = [
-  { id: 1, item: 'eggs', quantity: 12, completed: false },
-  { id: 2, item: 'milk', quantity: 2, completed: true },
-  { id: 3, item: 'chicken breast', quantity: 3, completed: false },
-  { id: 4, item: 'carrots', quantity: 6, completed: true }
-]
 
-const properties = [
-  { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
-  { id: 129331, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
-  { id: 129032, name: "Cactus Retreat", rating: 4.75, price: 300 },
-  { id: 129033, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
-  { id: 129034, name: "Oceanview Condo", rating: 4.7, price: 140 },
-  { id: 129035, name: "Gold Miner Campground", rating: 4.69, price: 96 },
-];
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* <PropertyList properties={properties} /> */}
-      <ColourBoxList colors={colors} />
+      <Counter />
 
 
 
 
 
+
+
+      {/* <PropertyList properties={properties} /> 
+      <ColourBoxList colors={colors} />*/}
       {/* <ShoppingList items={data} /> */}
       {/* <Slots val1="🍒" val2="🍌" val3="🍒" />
       <Slots val1="🍒" val2="🍒" val3="🍒" /> */}
---
+++ src/counter.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+export default function Counter() {
+    const [count, setCount] = useState(0)
+    const [name, setName] = useState("")
+    useEffect(function myEffect() {
+
+    }, [count])
+    const increment = () => {
+        setCount(c => (c + 1));
+    }
+    const handleChange = (e) => {
+        setName(e.target.value)
+    }
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={increment}>+1</button>
+            <p>Name: {name}</p>
+            <input value={name} onChange={handleChange} type="text" />
+        </div>
+    )
+}
